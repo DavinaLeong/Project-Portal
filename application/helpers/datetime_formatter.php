13ece0a3f938e0d328da3a2670ebac455827f4d6
Davina Leong <davina_leong@theshipyard.sg>
2017-01-26 18:29:47 +0800

    Added vendor resources.

--- application/helpers/datetime_formatter.php
+++ application/helpers/datetime_formatter.php
@@ -1,4 +1,15 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**********************************************************************************
+	- File Info -
+		File name		: datetime_formatter.jpg
+		Author(s)		: DAVINA Leong Shi Yun
+		Date Created	: 26 Jan 2016
+
+	- Contact Info -
+		Email	: leong.shi.yun@gmail.com
+		Mobile	: (+65) 9369 3752 [Singapore]
+
+***********************************************************************************/
 
 function now($format=SYSTEM_DATE_FORMAT)
 {
---
+++ application/models/User_log_model.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**********************************************************************************
+	- File Info -
+		File name		: User_log_model.jpg
+		Author(s)		: DAVINA Leong Shi Yun
+		Date Created	: 26 Jan 2016
+
+	- Contact Info -
+		Email	: leong.shi.yun@gmail.com
+		Mobile	: (+65) 9369 3752 [Singapore]
+
+***********************************************************************************/
+
+class User_log_model extends CI_Model
+{
+
+
+} // end User_log_model class
