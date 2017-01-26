b8cc561b356af3d2d3584397edad325f3e129ece
Davina Leong <davina_leong@theshipyard.sg>
2017-01-26 10:42:28 +0800

    Updated constants.

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,25 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+|--------------------------------------------------------------------------
+| Custom Constants
+|--------------------------------------------------------------------------
+*/
+
+define('RESOURCES_FOLDER', 'http://localhost/_project_index/resources/');
+define('SYSTEM_DATE_FORMAT', 'd M Y');
+define('DATE_TIME_ZONE', 'Asia/Singapore');
+
+
+// table names
+define('TABLE_USER', 'user');
+define('TABLE_USER_LOG', 'user_log');
+
+define('TABLE_PLATFORM', 'platform');
+define('TABLE_PROJECT_CATEGORY', 'project_category');
+define('TABLE_PROJECT', 'project');
+
+define('TABLE_LINK_CATEGORY', 'link_category');
+define('TABLE_LINK', 'link');
