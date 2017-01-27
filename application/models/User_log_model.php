52e835a9050e31774d188a202f77d176ba3c576c
Davina Leong <davina_leong@theshipyard.sg>
2017-01-27 12:05:06 +0800

    Implemented User and User Log table.

--- application/models/User_log_model.php
+++ application/models/User_log_model.php
@@ -13,6 +13,74 @@
 
 class User_log_model extends CI_Model
 {
+    public function get_all()
+    {
+        $this->db->order_by('timestamp', 'DESC');
+        $query = $this->db->get(TABLE_USER_LOG);
+        return $query->result_array();
+    }
 
+    public function log_message($message=FALSE)
+    {
+        if(isset($message))
+        {
+            $temp_array = array(
+                'user_id' => $this->session->userdata('user_id'),
+                'message' => $message
+            );
+
+            $now = now();
+            $this->db->set('timestamp', now('c'));
+            $this->db->insert(TABLE_USER_LOG, $temp_array);
+            return $this->db->insert_id();
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function validate_access($required=FALSE, $session=FALSE)
+    {
+        if(isset($required) && isset($session))
+        {
+            $valid = FALSE;
+            for($i = 0; $i < strlen($session); ++$i)
+            {
+                if(strpos($required, substr($session, $i, 1)) !== FALSE)
+                {
+                    $valid = TRUE;
+                    break;
+                }
+            }
+            return $valid;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function validate_admin_access()
+    {
+        if($this->validate_access('A', $this->session->userdata('access')) == FALSE)
+        {
+            $this->session->set_userdata('message', 'This user has invalid access rights to requested page.');
+            redirect('admin/authenticate/logout');
+        }
+    }
+
+    public function get_by_user_id($user_id=FALSE)
+    {
+        if($user_id)
+        {
+            $query = $this->db->get_where($user_id);
+            return $query->result_array();
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
 
 } // end User_log_model class
---
+++ application/models/User_model.php
@@ -0,0 +1,104 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**********************************************************************************
+	- File Info -
+		File name		: User_model.jpg
+		Author(s)		: DAVINA Leong Shi Yun
+		Date Created	: 27 Jan 2016
+
+	- Contact Info -
+		Email	: leong.shi.yun@gmail.com
+		Mobile	: (+65) 9369 3752 [Singapore]
+
+***********************************************************************************/
+
+class User_model extends CI_Model
+{
+    public function count_all()
+    {
+        return $this->db->count_all(TABLE_USER);
+    }
+
+	public function get_all($column='last_updated', $direction='DESC')
+    {
+        $this->db->order_by($column, $direction);
+        $query = $this->db->get(TABLE_USER);
+        return $query->result_array();
+    }
+
+    public function get_by_user_id($user_id=FALSE)
+    {
+        if($user_id)
+        {
+            $query = $this->db->get_where($user_id);
+            return $query->row_array();
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function insert($user=FALSE)
+    {
+        if($user)
+        {
+            $temp_array = array(
+                'username' => $user['username'],
+                'name' => $user['name'],
+                'password_hash' => $user['password_hash'],
+                'access' => $user['access'],
+                'status' => $user['status']
+            );
+
+            $this->db->set('date_added', now('c'));
+            $this->db->set('last_updated', now('c'));
+            $this->db->insert(TABLE_USER, $temp_array);
+            return $this->db->insert_id();
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function update($user=FALSE)
+    {
+        if($user)
+        {
+            $temp_array = array(
+                'user_id' => $user['user_id'],
+                'username' => $user['username'],
+                'name' => $user['name'],
+                'password_hash' => $user['password_hash'],
+                'access' => $user['access'],
+                'status' => $user['status']
+            );
+
+            $this->db->set('last_updated', now('c'));
+            $this->db->update(TABLE_USER, $temp_array, $temp_array['user_id']);
+            return $this->db->affected_rows();
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function _access_array()
+    {
+        return array(
+            'Administrator' => 'A',
+            'Manager' => 'M',
+            'User' => 'U'
+        );
+    }
+
+    public function _status_array()
+    {
+        return array(
+            'Activated',
+            'Deactivated'
+        );
+    }
+
+} // end User_model class
