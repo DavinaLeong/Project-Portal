d91d94af598f1b9f909aa25bdb8bb2df134f3464
Davina Leong <leong.shi.yun@gmail.com>
2017-01-28 15:42:29 +0800

    Implemented Authenticate controller.

--- application/controllers/admin/Authenticate.php
+++ application/controllers/admin/Authenticate.php
@@ -17,7 +17,7 @@ class Authenticate extends CI_Controller
 	{
 		parent::__construct();
 	}
-	
+
 	public function index()
 	{
 	    redirect('admin/authenticate/start');
@@ -25,17 +25,70 @@ class Authenticate extends CI_Controller
 
     public function login()
     {
-        $this->load->view('admin/authenticate/login_page');
+        if($this->session->userdata('user_id') || $this->session->userdata('access'))
+		{
+			$this->User_log_model->log_message('User has been logged out.');
+			$this->session->unset_userdata('user_id');
+			$this->session->unset_userdata('username'
+		);
+			$this->session->unset_userdata('name');
+			$this->session->unset_userdata('access');
+			$this->session->set_userdata('message', 'You have been logged out.');
+		}
+
+		$this->_set_rules_login();
+		if($this->form_validation->runw())
+		{
+			if($user = $this->User_model->get_by_username($this->input->post('username')))
+			{
+				if($user['status'] == 'Activated')
+				{
+					if(password_verify($this->input->post('password'), $user['password_hash']))
+					{
+						$this->_set_userdata();
+						$this->User_log_model->log_message('User has logged in.');
+						$this->session->set_userdata('message', 'Login successful.');
+					}
+					else
+					{
+						$this->session->set_userdata('message', 'Password is incorrect.');
+					}
+				}
+				else
+				{
+					$this->session->set_userdata('message', 'This account has been deactivated.');
+				}
+			}
+			else
+			{
+				$this->session->set_userdata('message', 'Invalid Username');
+			}
+		}
     }
+
+	private function _set_rules_login()
+	{
+		$this->form_validaton->set_rules('username', 'Username', 'trim|required|max_length[512]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|max_length[512]');
+	}
+
+	private function _set_userdata()
+	{
+		$this->session->set_userdata('user_id', $user['user_id']);
+		$this->session->set_userdata('username', $user['username']);
+		$this->session->set_userdata('name', $user['name']);
+		$this->session->set_userdata('access', $user['access']);
+	}
 
     public function logout()
     {
-
+		redirect('admin/authenticate/login');
     }
 
     public function start()
     {
+		$this->User_log_model->validate_access();
         $this->load->view('admin/authenticate/start_page');
     }
-	
+
 } // end Test controller class
